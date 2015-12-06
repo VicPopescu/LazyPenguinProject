33db34a46fb843855539d183c40d289d277a9314
Victor Popescu <nomevictor@gmail.com>
2015-12-06 18:30:32 +0200

    Update, rearrange and clean code

--- main.cpp
+++ main.cpp
@@ -1,67 +1,26 @@
-#include<SDL.h> //access SDL functions
+#include "Game.h"
+#include "Windows.h"
+
+// creating the Game object
+Game* g_game = 0;
 
 
-
-/* GLOBAL VARIABLES */
-SDL_Window* g_pWindow = 0; // pointer to an SDL_Window function, which will be set using the SDL_CreateWindow function
-SDL_Renderer* g_pRenderer = 0; //pointer to an SDL_Renderer object
-
-bool g_bRunning = false; // this will create a loop
-
-
-/* SDL INITIALIZATION */
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags){
-
-    // initialize SDL
-    if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
-
-    // if succeeded create our window
-    g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
-
-    // if the window creation succeeded create our renderer
-
-        if(g_pWindow != 0){
-
-        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-    }else{
-
-    return false; // SDL could not initialize
-    }
-
-return true;
-}
-
-/* RENDERING FUNCTION */
-void render(){
-
-    // set to blue
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 0);
-    // clear the window to blue
-    SDL_RenderClear(g_pRenderer);
-    // show the window
-    SDL_RenderPresent(g_pRenderer);
-}
 
 /* MAIN FUNCTION */
-int main(int argc, char* args[]){
+int main(int argc, char* argv[]){
 
-    if(init("Lazy Penguin",
-    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,
-    480, SDL_WINDOW_SHOWN)){
+    g_game = new Game();
+    g_game->init("Lazy Penguin", 100, 100, 640, 480, 0);
 
-        g_bRunning = true;
-    }else{
+    while(g_game->running()){
 
-    return 1; //something's wrong
+    g_game->handleEvents();
+    //g_game->update();
+    g_game->render();
     }
 
-    while(g_bRunning){
-
-    render();
-    }
-    // clean up SDL
-    SDL_Quit();
+    g_game->clean();
     return 0;
+
 }
 
