a04fb60099a8e5be059a6d62e70baf3cbdf6e44e
Victor Popescu <nomevictor@gmail.com>
2015-12-08 03:45:07 +0200

    Code restructure

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -52,19 +52,17 @@ void TextureManager::draw(std::string id, int x, int y, int width, int height, S
 
 
 /* DRAW FRAME */
-void TextureManager::drawFrame(std::string id, int x, int y, int
-width, int height, int currentRow, int currentFrame, SDL_Renderer
-*pRenderer, SDL_RendererFlip flip)
-{
-SDL_Rect srcRect;
-SDL_Rect destRect;
-srcRect.x = width * currentFrame;
-srcRect.y = height * (currentRow - 1);
-srcRect.w = destRect.w = width;
-srcRect.h = destRect.h = height;
-destRect.x = x;
-destRect.y = y;
-SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect,
-&destRect, 0, 0, flip);
+void TextureManager::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *pRenderer, SDL_RendererFlip flip){
+
+    SDL_Rect srcRect;
+    SDL_Rect destRect;
+    srcRect.x = width * currentFrame;
+    srcRect.y = height * (currentRow - 1);
+    srcRect.w = destRect.w = width;
+    srcRect.h = destRect.h = height;
+    destRect.x = x;
+    destRect.y = y;
+    SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip);
+
 }
 
