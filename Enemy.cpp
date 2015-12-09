deddcba79304701cc37833121166f06c638ca49f
Victor Popescu <nomevictor@gmail.com>
2015-12-09 07:17:30 +0200

    Frame cap and frame starting point working

--- Enemy.cpp
+++ Enemy.cpp
@@ -18,7 +18,7 @@ void Enemy::draw(){
 void Enemy::update(){
 
     m_x -= 1;
-    m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+    m_currentFrame = int((3 +(SDL_GetTicks() / 100) % 3));
 }
 
 
