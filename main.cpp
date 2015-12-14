c8439a34e3c9fccac2ed0c1fb6e2dcf09d905120
Victor Popescu <nomevictor@gmail.com>
2015-12-14 13:36:54 +0200

    Creating fixed fps
    -Custom delay function
    +Creating a fixed 60 fps loop

--- main.cpp
+++ main.cpp
@@ -6,10 +6,17 @@
 // creating the Game object
 Game* g_game = 0;
 
+//FPS constants
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS; //how much we delay between loops to get 60 fps
+
 
 
 /* MAIN FUNCTION */
 int main(int argc, char* argv[]){
+
+    //fps calculation variables
+    Uint32 frameStart, frameTime;
 
     std::cout << "game init attempt...\n";
 
@@ -19,10 +26,22 @@ int main(int argc, char* argv[]){
 
         while(TheGame::Instance()->running()){
 
+            //get the time and store it in frameStart
+            frameStart = SDL_GetTicks();
+
+            //run game loop
             TheGame::Instance()->handleEvents();
             TheGame::Instance()->update();
             TheGame::Instance()->render();
-            SDL_Delay(20);
+
+            //store how long it took to run, by subtracting the time our frame started from current time
+            frameTime = SDL_GetTicks() - frameStart;
+
+            //if it's less then the time we want a frame to take, we call SDL_Delay to make our loop wait
+            if(frameTime < DELAY_TIME){
+                //subtracting how long the loop already took to complete and delay the rest of the time
+                SDL_Delay((int)(DELAY_TIME - frameTime));
+            }
         }
     }else{
 
