bd94c2286ce290d44917a9102b699c5b7387f160
Victor Popescu <nomevictor@gmail.com>
2015-12-14 10:49:35 +0200

    Adding Vector2D class

---
+++ Vector2D.h
@@ -0,0 +1,27 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+
+#include <math.h>//using math operations
+
+
+class Vector2D
+{
+public:
+
+        Vector2D(float x, float y): m_x(x), m_y(y){}
+
+        float getX() {return m_x;}
+        float getY() {return m_y;}
+
+        void setX(float x) {m_x = x;}
+        void setY(float y) {m_y = y;}
+
+
+private:
+
+        float m_x;
+        float m_y;
+};
+
+#endif // VECTOR2D_H
