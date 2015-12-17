078be1b1fc0cb7316adc83e94fc7830e83983dcc
Victor Popescu <nomevictor@gmail.com>
2015-12-17 13:04:47 +0200

    Update FSM.cpp
    +changeState function

--- FSM.cpp
+++ FSM.cpp
@@ -1,6 +1,7 @@
 #include "FSM.h"
 
 
+/* pushState */
 void FSM::pushState(GameState *pState)
 {
     m_gameStates.push_back(pState);
@@ -8,7 +9,7 @@ void FSM::pushState(GameState *pState)
 
 }
 
-
+/* popState */
 void FSM::popState()
 {
     //check if there are any states available to remove
@@ -24,9 +25,30 @@ void FSM::popState()
 
 }
 
-
+/* changeState */
 void FSM::changeState(GameState *pState)
 {
+    //check if there are already any states in the array
+    if(!m_gameStates.empty())
+    {
+        //if there are, check whether their state ID is the same as the current one
+        if(m_gameStates.back()->getStateID() == pState->getStateID())
+        {
+            //if it is:
+            return; //don't do anything
+        }
+        //if the state ID doesn't match
+        if(m_gameStates.back()->onExit())
+        {
+            //remove current state
+            delete m_gameStates.back();
+            m_gameStates.pop_back();
+        }
+    }
 
+    //then push back our new state
+    m_gameStates.push_back(pState);
+    //initialize it
+    m_gameStates.back()->onEnter();
 
 }
