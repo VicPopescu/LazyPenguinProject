5e3bd41e57cc13682adb27db1515ee5f7156023c
Victor Popescu <nomevictor@gmail.com>
2015-12-17 10:18:43 +0200

    Adding GameStates.h abstract class

---
+++ GameState.h
@@ -0,0 +1,63 @@
+#ifndef GAMESTATE_H_INCLUDED
+#define GAMESTATE_H_INCLUDED
+
+#include <string>
+
+/*
+*   Handling game states.
+*   This abstract base class is used to implement a blueprint
+*   that will be inherited and followed by our derived classes.
+*
+*/
+
+
+
+
+class GameState
+{
+
+public:
+        virtual void onEnter() = 0;
+        virtual void onExit() = 0;
+
+        virtual void update() = 0;
+        virtual void render() = 0;
+
+        virtual std::string getStateID() const = 0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // GAMESTATE_H_INCLUDED
