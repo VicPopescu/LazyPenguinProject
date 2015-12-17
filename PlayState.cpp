ec07974fcefbbf4087a061b0287ca2c06832fd34
Victor Popescu <nomevictor@gmail.com>
2015-12-17 10:50:04 +0200

    Implementing PLayState.cpp functions

--- PlayState.cpp
+++ PlayState.cpp
@@ -1,2 +1,28 @@
 #include "PlayState.h"
 
+
+
+const std::string PlayState::s_playID = "PLAY";
+
+
+bool onEnter()
+{
+    std::cout << "Entering PlayState \n";
+    return true;
+}
+
+bool onExit()
+{
+    std::cout << "Exiting PlayState \n";
+    return true;
+}
+
+void update()
+{
+    //nothing here yet
+}
+
+void render()
+{
+    //nothing here yet
+}
