5b223e656bab2cda35e3a9dadbed679c6dcf4c9f
Victor Popescu <nomevictor@gmail.com>
2015-12-17 10:24:46 +0200

    Adding MenuState classes

---
+++ MenuState.h
@@ -0,0 +1,14 @@
+#ifndef MENUSTATE_H
+#define MENUSTATE_H
+
+
+class MenuState
+{
+    public:
+        MenuState();
+        ~MenuState();
+    protected:
+    private:
+};
+
+#endif // MENUSTATE_H
