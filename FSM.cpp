110d0b9dfc5ff03ee56d7ee942eb2574a0101f7e
Victor Popescu <nomevictor@gmail.com>
2015-12-17 12:53:21 +0200

    Update FSM.cpp
    +popState function

--- FSM.cpp
+++ FSM.cpp
@@ -3,17 +3,30 @@
 
 void FSM::pushState(GameState *pState)
 {
+    m_gameStates.push_back(pState);
+    m_gameStates.back()->onEnter();
 
 }
 
 
 void FSM::popState()
 {
+    //check if there are any states available to remove
+    if(!m_gameStates.empty())
+    {
+        //if there are, call onExit of the current state and remove it
+        if(m_gameStates.back()->onExit())
+        {
+            delete m_gameStates.back();
+            m_gameStates.pop_back();
+        }
+    }
 
 }
 
 
 void FSM::changeState(GameState *pState)
 {
+
 
 }
