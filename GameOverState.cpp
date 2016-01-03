3e7175167e25242c29d84074316206421ff4455a
Victor Popescu <nomevictor@gmail.com>
2016-01-03 11:07:09 +0200

    Added GameOverState class
    This class will handle the game over state

---
+++ GameOverState.cpp
@@ -0,0 +1,11 @@
+#include "GameOverState.h"
+
+GameOverState::GameOverState()
+{
+    //ctor
+}
+
+GameOverState::~GameOverState()
+{
+    //dtor
+}
---
+++ GameOverState.h
@@ -0,0 +1,14 @@
+#ifndef GAMEOVERSTATE_H
+#define GAMEOVERSTATE_H
+
+
+class GameOverState
+{
+    public:
+        GameOverState();
+        ~GameOverState();
+    protected:
+    private:
+};
+
+#endif // GAMEOVERSTATE_H
