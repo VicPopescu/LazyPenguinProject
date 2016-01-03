9ad6e31bb2a6a9b45da3d18064fddb914356bd6e
Victor Popescu <nomevictor@gmail.com>
2016-01-03 11:38:33 +0200

    Update GameOverState.cpp

--- GameOverState.cpp
+++ GameOverState.cpp
@@ -2,11 +2,18 @@
 #include "Game.h"
 #include "MenuState.h"
 #include "PlayState.h"
+#include "MenuButtons.h"
+#include "AnimatedGraphic.h"
+
+
 
 //define static variable
 const std::string GameOverState::s_gameOverID = "GAMEOVER";
 
-//define static function
+
+
+
+//define static functions
 void GameOverState::s_gameOverToMain()
 {
 
@@ -14,7 +21,6 @@ void GameOverState::s_gameOverToMain()
 
 }
 
-//define static function
 void GameOverState::s_restartPlay()
 {
 
@@ -22,3 +28,56 @@ void GameOverState::s_restartPlay()
 
 }
 
+
+
+
+
+
+bool GameOverState::onEnter()
+{
+    if(!TheTextureManager::Instance()->load("Resources/gameover.png", "gameovertext", TheGame::Instance()->getRenderer()))
+    {
+        return false;
+    }
+
+    if(!TheTextureManager::Instance()->load("Resources/main-menu.png", "mainbutton", TheGame::Instance()->getRenderer()))
+    {
+        return false;
+    }
+
+    if(!TheTextureManager::Instance()->load("Resources/restart.png", "restartbutton", TheGame::Instance()->getRenderer()))
+    {
+        return false;
+    }
+
+
+    GameObject* gameOverText = new AnimatedGraphic(new LoaderParams(200, 100, 190, 30, "gameovertext"), 2);
+    GameObject* button1 = new MenuButtons(new LoaderParams(200, 200, 200, 80, "mainbutton"), s_gameOverToMain);
+    GameObject* button2 = new MenuButtons(new LoaderParams(200, 300, 200, 80, "restartbutton"), s_restartPlay);
+
+    m_gameObjects.push_back(gameOverText);
+    m_gameObjects.push_back(button1);
+    m_gameObjects.push_back(button2);
+
+    std::cout << "Enter PauseState\n";
+    return true;
+
+}
+
+
+bool GameOverState::onExit()
+{
+
+}
+
+
+void GameOverState::update()
+{
+
+}
+
+
+void GameOverState::render()
+{
+
+}
