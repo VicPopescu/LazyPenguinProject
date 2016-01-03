2aa537455ebd59d9aee4c54b16e4d4db7ca00251
Victor Popescu <nomevictor@gmail.com>
2016-01-03 11:20:11 +0200

    Update

--- GameOverState.cpp
+++ GameOverState.cpp
@@ -1,2 +1,24 @@
 #include "GameOverState.h"
+#include "Game.h"
+#include "MenuState.h"
+#include "PlayState.h"
+
+//define static variable
+const std::string GameOverState::s_gameOverID = "GAMEOVER";
+
+//define static function
+void GameOverState::s_gameOverToMain()
+{
+
+    TheGame::Instance()->getStateMachine()->changeState(new MenuState());
+
+}
+
+//define static function
+void GameOverState::s_restartPlay()
+{
+
+    TheGame::Instance()->getStateMachine()->changeState(new PlayState());
+
+}
 
